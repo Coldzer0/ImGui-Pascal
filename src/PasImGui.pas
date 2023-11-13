c6a8f13cd86eaa3331b4f6d4adaafd9ef2e34d5d
Coldzer0 <6647006+Coldzer0@users.noreply.github.com>
2023-11-13 04:25:44 +0200

    Changing the LICENSE to LGPL

--- src/PasImGui.pas
+++ src/PasImGui.pas
@@ -4,13 +4,13 @@
   Copyright (C) 2023 Coldzer0 <Coldzer0 [at] protonmail.ch>
 
   This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
+  it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
   the Free Software Foundation, version 3 of the License.
 
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  GNU LESSER GENERAL PUBLIC LICENSE for more details.
 }
 
 Unit PasImGui;
